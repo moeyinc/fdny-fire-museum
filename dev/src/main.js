5f5b1a33289f97b4bac1ae535bd8fc5f204f3043
Seiyable <sk4525@nyu.edu>
2018-08-08 22:24:42 -0400

    fix material design icon style

--- dev/src/main.js
+++ dev/src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import MixinGeneral from '@/mixins/general.js'
-import 'vue-material-design-icons/styles.css'
+// import 'vue-material-design-icons/styles.css'
 import VueTouch from 'vue-touch'
 
 Vue.config.productionTip = false
